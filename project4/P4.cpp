1962ac1cca027ae2e91ff5bcd66e41d1ad80bacf
Lam Ngo <103980343+lammngo@users.noreply.github.com>
2022-05-20 20:40:29 -0700

    Update P4.cpp

--- project4/P4.cpp
+++ project4/P4.cpp
@@ -1,83 +1,267 @@
+//
+// Created by lolita kim on 5/12/22.
+//
+
 #include <iostream>
+#include <memory>
 #include "inFest.h"
+#include "gridFlea.h"
+#include "vector"
 
 using namespace std;
 
 const int NUM_INFESTS = 10;
 
-inFest **createInfests() {
-    inFest **infests = new inFest *[NUM_INFESTS];
+vector<unique_ptr<inFest>> createInfests() {
+    vector<unique_ptr<inFest>> infests(NUM_INFESTS);
     for (int i = 0; i < NUM_INFESTS; i++) {
         int num_gen = rand() % 10 + 1;
-        int len = rand() % 10 + 1;
-        infests[i] = new inFest(num_gen, len);
+        int len = rand() % 20 + 1;
+        infests[i] = make_unique<inFest>(num_gen, len);
     }
     return infests;
 }
 
-void deleteInfests(inFest **infests) {
-    for (int i = 0; i < NUM_INFESTS; i++) {
-        delete infests[i];
+void createSharedInfests() {
+    int num_gen = rand() % 10 + 1;
+    int len = rand() % 20 + 1;
+    shared_ptr<inFest>shared_ptr_infest = make_shared<inFest>(num_gen, len);
+    cout << endl << "=== TESTS ON SHARED POINTERS ===" << endl
+         << "/ this test will create an array of shared pointers points to the same object address /" << endl
+         << "- Original object address: " << shared_ptr_infest.get() << endl
+         << "- Original reference count: " << shared_ptr_infest.use_count() << endl
+         << "================================================" << endl;
+
+    vector<shared_ptr<inFest>> shared_infests(6);
+    for(int i = 0; i < shared_infests.size(); i++){
+        shared_infests[i] = shared_ptr_infest;
+        cout << "Shared infest #" << (i + 1) << endl
+             << "- Object address: " << shared_infests[i].get() << endl
+             << "- Reference count: " << shared_infests[i].use_count() << endl
+             << "=============================================" << endl;
     }
-    delete[] infests;
+    cout << "<<< RESULTS AFTER USING SHARED POINTERS >>>" << endl
+         << "- Final object address: " << shared_ptr_infest.get() << endl
+         << "- Final reference count: " << shared_ptr_infest.use_count() << endl;
+}
+void testSharedInfest(){
+    int num_gen = rand() % 10 + 1;
+    int len = rand() % 20 + 1;
+    shared_ptr<inFest>shared_ptr_infest = make_shared<inFest>(num_gen, len);
+    cout << endl << "=== OUT OF SCOPE TESTS ON SHARED POINTERS ===" << endl
+         << "/ this test will reduce the reference count by how many shared infests went out of scope /" << endl
+         << "- Original object address: " << shared_ptr_infest.get() << endl
+         << "- Original reference count: " << shared_ptr_infest.use_count() << endl
+         << "================================================" << endl;
+
+    vector<shared_ptr<inFest>> shared_infests(6);
+    for(int i = 0; i < shared_infests.size(); i++){
+        shared_infests[i] = shared_ptr_infest;
+        cout << "Shared infest #" << (i + 1) << endl
+             << "- Object address: " << shared_infests[i].get() << endl
+             << "- Reference count: " << shared_infests[i].use_count() << endl
+             << "=============================================" << endl;
+    }
+    cout << "...MAKING THE OG OBJECT OUT OF SCOPE..." << endl;
+    shared_ptr_infest.reset();
+    cout << "Updated the new state for the original object!" << endl
+         << "=============================================" << endl;
+
+    for(int i = 0; i < shared_infests.size(); i++){
+        shared_infests[i] = shared_ptr_infest;
+        cout << "Shared infest #" << (i + 1) << endl
+             << "- Object address: " << shared_infests[i].get() << endl
+             << "- Reference count: " << shared_infests[i].use_count() << endl;
+        shared_infests[i].reset();
+        cout << "=============================================" << endl;
+    }
+
+    cout << "<<< RESULTS AFTER MAKING SHARED POINTERS OUT OF SCOPE >>>" << endl
+         << "- Final object address: " << shared_ptr_infest.get() << endl
+         << "- Final reference count: " << shared_ptr_infest.use_count() << endl;
+
+}
+//using unique pointers
+void testInfestRelation(vector<unique_ptr<inFest>> &infest){
+    cout << endl << " === RELATIONS TESTS ON 2 INITIAL OBJECTS ===" << endl
+         << "infest #1: " << infest[0]->getValue() << endl
+         << "infest #2: " << infest[1]->getValue() << endl
+         << ">>> RESULTS CHART <<< " << endl
+         << "infest1 == infest2: " << (infest[0]->getValue() == infest[1]->getValue() ? "true" : "false") << endl
+         << "infest1 != infest2: " << (infest[0]->getValue() != infest[1]->getValue() ? "true" : "false") << endl
+         << "infest1 > infest2: " << (infest[0]->getValue() > infest[1]->getValue() ? "true" : "false") << endl
+         << "infest1 < infest2: " << (infest[0]->getValue() < infest[1]->getValue() ? "true" : "false") << endl
+         << "infest1 >= infest2: " << (infest[0]->getValue() >= infest[1]->getValue() ? "true" : "false") << endl
+         << "infest1 <= infest2: " << (infest[0]->getValue() <= infest[1]->getValue() ? "true" : "false") << endl;
+
+    infest[0]->move(2);
+    infest[1]->move(3);
+
+    cout << endl << " === RELATIONS TESTS ON 2 MOVING OBJECTS === " << endl
+         << "infest #1 moving 2 squares..." << endl
+         << "infest #2 moving 3 squares..." << endl
+         << "/// Updated new values for 2 objects /// " << endl
+         << "infest #1: " << infest[0]->getValue() << endl
+         << "infest #2: " << infest[1]->getValue() << endl
+         << ">>> RESULTS CHART <<<" << endl
+         << "infest1 == infest2: " << (infest[0]->getValue() == infest[1]->getValue() ? "true" : "false") << endl
+         << "infest1 != infest2: " << (infest[0]->getValue() != infest[1]->getValue() ? "true" : "false") << endl
+         << "infest1 > infest2: " << (infest[0]->getValue() > infest[1]->getValue() ? "true" : "false") << endl
+         << "infest1 < infest2: " << (infest[0]->getValue() < infest[1]->getValue() ? "true" : "false") << endl
+         << "infest1 >= infest2: " << (infest[0]->getValue() >= infest[1]->getValue() ? "true" : "false") << endl
+         << "infest1 <= infest2: " << (infest[0]->getValue() <= infest[1]->getValue() ? "true" : "false") << endl;
 }
 
-void printCallByValue(inFest infest){
-    cout << "Min value: " << infest.minValue() << endl;
-    cout << "Max value: " << infest.maxValue() << endl;
+void testAddInfests(vector<unique_ptr<inFest>> &infest, inFest newInfest){
+    cout << endl << "=== ADDITION INFEST ON INFEST ARRAY === " << endl
+         << "/ this test will append new infest (multiple gridFleas) on each infest / " << endl
+         << "- Original infest size is " << infest.size() << endl
+         << "- Adding the infest size of " << newInfest.getSize() << endl
+         << ">>> Adding the infest value of " << newInfest.getValue() << " <<<" << endl
+         << endl << "*** RESULTS CHART ***" << endl;
+
+    int i = 0;
+    for (unique_ptr<inFest>& infestptr : infest)
+    {
+        cout << "Old infest #" << (i) << " size: " << infestptr->getSize() << endl;
+        cout << "Old infest #" << (i) << " value: " << infestptr->getValue() << endl;
+
+        (*infestptr) += newInfest;
+
+        cout << "New infest #" << (i) << " size: " << infestptr->getSize() << endl;
+        cout << "New infest #" << (i) << " value: " << infestptr->getValue() << endl;
+        i++;
+        cout << "==============================" << endl;
+    }
 }
 
-void run(inFest &infest, int index){
-    cout << "==========================================" << endl;
-    cout << "<<< Object #" << index << " attempting to run 2 times >>>" << endl;
-    infest.move(2);
-    cout << "Min value: " << infest.minValue() << endl;
-    cout << "Max value: " << infest.maxValue() << endl;
-    cout << "<<< Object #" << index << " attempting to run 5 times >>>" << endl;
-    infest.move(5);
-    cout << "Min value: " << infest.minValue() << endl;
-    cout << "Max value: " << infest.maxValue() << endl;
-    cout << "==========================================" << endl;
-    inFest copiedInfest = infest;
-    cout << ">>> Created a copy of object #" << index << " <<<" << endl;
-    cout << "Min value (og infest): " << infest.minValue() << endl;
-    cout << "Max value (og infest): " << infest.minValue() << endl;
-    cout << "Min value (copied infest): " << copiedInfest.minValue() << endl;
-    cout << "Max value (copied infest): " << copiedInfest.minValue() << endl;
+void testAddGridFlea(vector<unique_ptr<inFest>> &infest, gridFlea gf){
+    cout << endl << "=== ADDITION GRID FLEA ON INFEST ARRAY === " << endl
+         << "/ this test will append a single new gridFlea on each infest / " << endl
+         << "- Original infest size is " << infest.size() << endl
+         << "- Adding the grid value of " << gf.value() << endl
+         << endl << "*** RESULTS CHART ***" << endl;
 
-    cout << "=== Attempting to move both infest objects by 3 times ===" << endl;
-    infest.move(3);
-    cout << "Min value (og infest): " << infest.minValue() << endl;
-    cout << "Max value (og infest): " << infest.minValue() << endl;
-    copiedInfest.move(3);
-    cout << "Min value (copied infest): " << copiedInfest.minValue() << endl;
-    cout << "Max value (copied infest): " << copiedInfest.minValue() << endl;
+    int i = 0;
+    for (unique_ptr<inFest>& infestptr : infest)
+    {
+        cout << "Old infest #" << (i) << " size: " << infestptr->getSize() << endl;
+        cout << "Old infest #" << (i) << " value: " << infestptr->getValue() << endl;
 
-    cout << "=== Attempting to move the og infest object by 4 times ===" << endl;
-    infest.move(4);
-    cout << "Min value: " << infest.minValue() << endl;
-    cout << "Max value: " << infest.maxValue() << endl;
+        (*infestptr) += gf;
 
-    cout << "=== Attempting to move the copied infest object by 2 times ===" << endl;
-    copiedInfest.move(2);
-    cout << "Min value: " << copiedInfest.minValue() << endl;
-    cout << "Max value: " << copiedInfest.maxValue() << endl;
+        cout << "New infest #" << (i) << " size: " << infestptr->getSize() << endl;
+        cout << "New infest #" << (i) << " value: " << infestptr->getValue() << endl;
+        i++;
+        cout << "==============================" << endl;
+    }
+}
 
-    cout << "Printing out the copied infest object called by value..." << endl;
-    printCallByValue(copiedInfest);
+void testGridRelations(gridFlea gf1, gridFlea gf2){
+    cout << endl << " === RELATIONS TESTS ON 2 INITIAL GRIDFLEAS ===" << endl
+         << "gridFlea #1: " << gf1.value() << endl
+         << "gridFlea #2: " << gf2.value() << endl
+         << ">>> RESULTS CHART <<< " << endl
+         << "gridFlea1 == gridFlea2: " << (gf1 == gf2 ? "true" : "false") << endl
+         << "gridFlea1 != gridFlea2: " << (gf1 != gf2 ? "true" : "false") << endl
+         << "gridFlea1 > gridFlea2: " << (gf1 > gf2 ? "true" : "false") << endl
+         << "gridFlea1 < gridFlea2: " << (gf1 < gf2 ? "true" : "false") << endl
+         << "gridFlea1 >= gridFlea2: " << (gf1 >= gf2 ? "true" : "false") << endl
+         << "gridFlea1 <= gridFlea2: " << (gf1 <= gf2 ? "true" : "false") << endl;
 
-    cout << "==========================================" << endl;
-    cout << "~~~ Finished running object #" << index << " ~~~" << endl;
-    cout << endl;
+    gf1.move(3);
+    gf2.move(5);
+
+    cout << endl << " === RELATIONS TESTS ON 2 MOVING GRIDFLEAS ===" << endl
+         << "gridFlea #1 moving 3 squares..." << endl
+         << "gridFlea #2 moving 5 squares..." << endl
+         << "/// Updated new values for 2 gridFleas /// " << endl
+         << "gridFlea #1: " << gf1.value() << endl
+         << "gridFlea #2: " << gf2.value() << endl
+         << ">>> RESULTS CHART <<< " << endl
+         << "gridFlea1 == gridFlea2: " << (gf1 == gf2 ? "true" : "false") << endl
+         << "gridFlea1 != gridFlea2: " << (gf1 != gf2 ? "true" : "false") << endl
+         << "gridFlea1 > gridFlea2: " << (gf1 > gf2 ? "true" : "false") << endl
+         << "gridFlea1 < gridFlea2: " << (gf1 < gf2 ? "true" : "false") << endl
+         << "gridFlea1 >= gridFlea2: " << (gf1 >= gf2 ? "true" : "false") << endl
+         << "gridFlea1 <= gridFlea2: " << (gf1 <= gf2 ? "true" : "false") << endl;
+}
+
+void testAddGridFlea(gridFlea gf, int steps){
+    cout << endl << "=== ADDITION TESTS ON GRID FLEA === " << endl
+         << "/ this test will move gridFlea up by injected value / " << endl
+         << "- Original gridFlea value is " << gf.value() << endl
+         << "- Adding to gridFlea a value of " << steps << " aka make gridFlea move up by " << steps << "" << endl;
+
+    gf += steps;
+    cout << ">>> New gridFlea value after moving up by " << steps << " is " << gf.value() << endl;
+}
+
+void testSubtractGridFlea(gridFlea gf, int steps){
+    cout << endl << "=== SUBTRACTION TESTS ON GRID FLEA === " << endl
+         << "/ this test will move gridFlea down by injected value / " << endl
+         << "- Original gridFlea value is " << gf.value() << endl
+         << "- Subtracting gridFlea a value of " << steps << " aka make gridFlea move down by " << steps << "" << endl;
+
+    gf -= steps;
+    cout << ">>> New gridFlea value after moving down by " << steps << " is -" << gf.value() << endl;
+}
+
+void testPrePostGridFlea(gridFlea gf){
+    cout << endl << "=== PRE-INCREMENT TEST ON GRID FLEA === " << endl
+         << "/ this test will move gridFlea up by 1 and return the updated the value / " << endl
+         << "- Original gridFlea value is " << gf.value() << endl;
+    ++gf;
+    cout << ">>> New gridFlea value after using pre-increment is " << gf.value() << endl;
+
+    cout << endl << "=== POST-INCREMENT TEST ON GRID FLEA === " << endl
+         << "/ this test will move gridFlea up by 1 and return the original the value / " << endl
+         << "- Original gridFlea value is " << gf.value() << endl;
+    gf++;
+    cout << ">>> New gridFlea value after using pre-increment is " << gf.value() << endl;
+}
+
+void testAddTwoGridFleas(gridFlea gf1, gridFlea gf2){
+    cout << endl << "=== ADDITION TESTS ON TWO MOVING GRID FLEAS === " << endl
+         << "/ this test will add 2 moving grid fleas together / " << endl;
+
+    gf1.move(2);
+    gf2.move(3);
+
+    cout << "Original gridFlea #1 value is " << gf1.value() << endl
+         << ">>> Adding on gridFlea #1 a value of " << gf2.value() << endl;
+    gf1 += gf2;
+    cout << "New gridFlea #1 value is " << gf1.value() << endl;
+}
+
+void testSubtractTwoGridFleas(gridFlea gf1, gridFlea gf2){
+    cout << endl << "=== SUBTRACTION TESTS ON TWO MOVING GRID FLEAS === " << endl
+         << "/ this test will subtract 2 moving grid fleas together / " << endl;
+
+    gf1.move(2);
+    gf2.move(3);
+
+    cout << "Original gridFlea #1 value is " << gf1.value() << endl
+         << ">>> Subtracting gridFlea #1 with a value of " << gf2.value() << endl;
+    gf1 -= gf2;
+    cout << "New gridFlea #1 value is -" << gf1.value() << endl;
 }
 
 int main(int argc, char *argv[]){
-    inFest **infests = createInfests();
-    cout << "*** Creating " << NUM_INFESTS << " infest objects ***" << endl;
-    cout << endl;
-
-    for(int i = 0; i < NUM_INFESTS; i++){
-        run(*infests[i], i + 1);
-    }
-    deleteInfests(infests);
+    vector<unique_ptr<inFest>> infests = createInfests();
+    gridFlea gf1 (3, 5, 10);
+    gridFlea gf2 (4,6,10);
+    cout << "                          <<< START HERE >>>" << endl;
+    cout << "Welcome! " << infests.size() << " infest objects has been created. You can run the tests now!" << endl;
+    testInfestRelation(infests);
+    testAddInfests(infests, *infests[0]);
+    testAddGridFlea(infests, gf1);
+    testGridRelations(gf1, gf2);
+    testAddGridFlea(gf2, 3);
+    testSubtractGridFlea(gf1, 4);
+    testPrePostGridFlea(gf1);
+    testAddTwoGridFleas(gf1, gf2);
+    testSubtractTwoGridFleas(gf1, gf2);
+    createSharedInfests();
+    testSharedInfest();
 }
