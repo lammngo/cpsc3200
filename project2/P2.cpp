08760116f75c8be4cf7808472f0d7786995a48e0
Lam Ngo <103980343+lammngo@users.noreply.github.com>
2022-05-20 20:39:23 -0700

    Update P2.cpp

--- project2/P2.cpp
+++ project2/P2.cpp
@@ -1,23 +1,33 @@
-//
-// Created by lolita kim on 4/11/22.
-//
-#include "inFest.h"
 #include <iostream>
+#include "inFest.h"
 
 using namespace std;
 
-int main(){
-    inFest j1(3, 5);
-    inFest j2(j1); //copy constructor
-    j1.move(2);
-    cout << "Min value of j1 is: " << j1.minValue() << endl;
-    cout << "Max value of j1 is: " << j1.maxValue() << endl;
-    j2.move(3);
-    inFest j3(2, 6);
-    j3.move(4);
-    inFest j4(std::move(j3)); //move assignment
-    
-    void run(inFest &infest, int index){
+const int NUM_INFESTS = 10;
+
+inFest **createInfests() {
+    inFest **infests = new inFest *[NUM_INFESTS];
+    for (int i = 0; i < NUM_INFESTS; i++) {
+        int num_gen = rand() % 10 + 1;
+        int len = rand() % 10 + 1;
+        infests[i] = new inFest(num_gen, len);
+    }
+    return infests;
+}
+
+void deleteInfests(inFest **infests) {
+    for (int i = 0; i < NUM_INFESTS; i++) {
+        delete infests[i];
+    }
+    delete[] infests;
+}
+
+void printCallByValue(inFest infest){
+    cout << "Min value: " << infest.minValue() << endl;
+    cout << "Max value: " << infest.maxValue() << endl;
+}
+
+void run(inFest &infest, int index){
     cout << "==========================================" << endl;
     cout << "<<< Object #" << index << " attempting to run 2 times >>>" << endl;
     infest.move(2);
@@ -59,6 +69,15 @@ int main(){
     cout << "==========================================" << endl;
     cout << "~~~ Finished running object #" << index << " ~~~" << endl;
     cout << endl;
+}
+
+int main(int argc, char *argv[]){
+    inFest **infests = createInfests();
+    cout << "*** Creating " << NUM_INFESTS << " infest objects ***" << endl;
+    cout << endl;
+
+    for(int i = 0; i < NUM_INFESTS; i++){
+        run(*infests[i], i + 1);
     }
-    return 0;
+    deleteInfests(infests);
 }
